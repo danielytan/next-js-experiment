63731585672b013eb40199a1104e404cb97ef6e1
Daniel <danielytan1@gmail.com>
2023-05-17 11:09:22 +0900

    completed polishing the index page step of dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,7 @@
+import Date from '../../components/date';
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
@@ -21,13 +24,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
