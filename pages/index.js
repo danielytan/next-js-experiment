7c7e7cfb3caeafe1b0f0785da4094b0ff3e26d75
Daniel <danielytan1@gmail.com>
2023-05-17 14:58:24 +0900

    slightly modified self intro section

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm learning front-end development!</p>
+        <p>Hi, I'm learning front-end development! This website uses Next.js, and hopefully Lit components as well soon! </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
